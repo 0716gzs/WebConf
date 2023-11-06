8c76e34cbcc3db98db4165c8e6a2e5e2aaf69aaf
jiaochanghao <jch_2154195820@163.com>
2023-11-06 20:48:59 +0800

    提交WebConf

---
+++ tests/nginx_test.py
@@ -0,0 +1,15 @@
+from WebConf.nginx import Nginx
+
+# 'nginx_conf_dir': '/opt/homebrew/etc/nginx',
+
+uwsgi_path = 'conf/uwsgi.ini'
+service_dir = '/Users/jiaochanghao/gitee_clone/django-rest-framework1/api'
+
+
+def main():
+    gen_service = Nginx(service_dir=service_dir, uwsgi_path=uwsgi_path)
+    gen_service.gen_nginx()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/uwsgi_test.py
@@ -0,0 +1,19 @@
+from WebConf.uwsgi import Uwsgi
+from WebConf.base_setting import BaseSetting
+
+
+uwsgi_conf = {
+    'service_dir': '/Users/jiaochanghao/gitee_clone/django-rest-framework/api',
+    'conf_dir': 'conf',
+    'virtualenv_dir': '/Users/jiaochanghao/.virtualenvs/api',
+}
+
+def main():
+    base = BaseSetting(service_dir='', conf_dir='', virtualenv_dir='')
+    gen_service = Uwsgi()
+    uwsgi_file = gen_service.gen_uwsgi()
+    print(uwsgi_file)
+
+
+if __name__ == "__main__":
+    main()
